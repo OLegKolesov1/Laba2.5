fff3b1c70f22d00bccd1464cb41275b30bdaacbc
Олег <91795895+OLegKolesov1@users.noreply.github.com>
2024-12-28 22:41:54 +0400

    Add files via upload

---
+++ laba2.js
@@ -0,0 +1,20 @@
+function factorial(n) 
+{
+    if (n === 0 || n === 1) 
+    {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+let n;
+do {
+    n = parseInt(prompt("Enter n:"));
+    if (n < 0) 
+    {
+        alert("n must be positive, try again:");
+    }
+} while (n < 0);
+
+let result = factorial(n);
+console.log(`${n}! = ${result}`);
